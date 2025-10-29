7878c205ce21ce92bea257482a3c872e411a6942
G <giovanni@MacBook-Pro-M4.local>
2025-10-29 16:36:10 +0300

    upd

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,4 +1,4 @@
-import { reactive, readonly } from 'vue'
+import { reactive } from 'vue'
 
 const state = reactive({
   name1: '',
@@ -8,44 +8,50 @@ const state = reactive({
   bgImage: ''
 })
 
-// при загрузке подставляем сохранённое состояние
+// при загрузке подтягиваем из localStorage
 const saved = localStorage.getItem('lover_chains_vue_state_v2')
 if (saved) {
   try {
     Object.assign(state, JSON.parse(saved))
   } catch (e) {
-    console.error('Ошибка чтения сохранённых данных', e)
+    console.error('Ошибка чтения данных', e)
   }
 }
 
+// сохраняем в localStorage при изменениях
+function save() {
+  localStorage.setItem('lover_chains_vue_state_v2', JSON.stringify(state))
+}
+
+// Методы изменения данных
 function setName1(v) {
   state.name1 = v
   save()
 }
+
 function setName2(v) {
   state.name2 = v
   save()
 }
+
 function setPhoto1(v) {
   state.photo1 = v
   save()
 }
+
 function setPhoto2(v) {
   state.photo2 = v
   save()
 }
+
 function setBgImage(v) {
   state.bgImage = v
   save()
 }
 
-function save() {
-  localStorage.setItem('lover_chains_vue_state_v2', JSON.stringify(state))
-}
-
 export function useStore() {
   return {
-    state: readonly(state),
+    state,
     setName1,
     setName2,
     setPhoto1,
