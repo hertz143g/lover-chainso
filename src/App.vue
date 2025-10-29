5f3f1900d504a65051110160cac8d5c261b2f413
G <giovanni@MacBook-Pro-M4.local>
2025-10-29 16:02:57 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<template>
+  <div class="min-h-screen bg-[#12001a] text-white">
+    <div class="mx-auto w-full max-w-[430px] min-h-screen pb-24">
+      <Header @open-settings="showSettings = true" />
+      <CoupleCircles />
+      <TogetherBlock />
+      <StatsGrid />
+    </div>
+
+    <!-- тут исправлено 👇 -->
+    <SettingsModal :show="showSettings" @close="showSettings = false" />
+  </div>
+</template>
+
 <script setup>
 import Header from '@/components/Header.vue'
 import CoupleCircles from '@/components/CoupleCircles.vue'
@@ -8,16 +22,3 @@ import { ref } from 'vue'
 
 const showSettings = ref(false)
 </script>
-
-<template>
-  <div class="min-h-screen bg-[#12001a] text-white">
-    <div class="mx-auto w-full max-w-[430px] min-h-screen pb-24">
-      <Header @open-settings="showSettings = true" />
-      <CoupleCircles />
-      <TogetherBlock />
-      <StatsGrid />
-    </div>
-
-    <SettingsModal v-if="showSettings" @close="showSettings = false" />
-  </div>
-</template>
