ae9cdaee0c079578f520a7df3666458abefa13f6
G <giovanni@MacBook-Pro-M4.local>
2025-10-29 15:24:36 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
-<template>
-  <div class="min-h-screen bg-[#12001a] text-white">
-    <div class="mx-auto w-full max-w-[430px] min-h-screen pb-24">
-      <Header />
-      <CoupleCircles />
-      <TogetherBlock />
-      <StatsGrid />
-    </div>
-  </div>
-</template>
 <script setup>
 import Header from '@/components/Header.vue'
 import CoupleCircles from '@/components/CoupleCircles.vue'
 import TogetherBlock from '@/components/TogetherBlock.vue'
 import StatsGrid from '@/components/StatsGrid.vue'
+import SettingsModal from '@/components/SettingsModal.vue'
+import { ref } from 'vue'
+
+const showSettings = ref(false)
 </script>
+
+<template>
+  <div class="min-h-screen bg-[#12001a] text-white">
+    <div class="mx-auto w-full max-w-[430px] min-h-screen pb-24">
+      <Header @open-settings="showSettings = true" />
+      <CoupleCircles />
+      <TogetherBlock />
+      <StatsGrid />
+    </div>
+
+    <SettingsModal v-if="showSettings" @close="showSettings = false" />
+  </div>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="w-full max-w-[430px] mx-auto px-6 pt-6 pb-2 flex items-center justify-between select-none z-50 relative"
+    class="w-full max-w-[430px] mx-auto px-6 pt-6 pb-2 flex items-center justify-between select-none relative z-50"
   >
     <button class="text-[13px] opacity-75 hover:opacity-100">магазин</button>
 
@@ -8,7 +8,6 @@
       <span class="text-pink-400">LOVER</span> CHAINSO
     </h1>
 
-    <!-- 👇 вот эта кнопка -->
     <button
       class="text-[13px] opacity-75 hover:opacity-100"
       @click="$emit('open-settings')"
