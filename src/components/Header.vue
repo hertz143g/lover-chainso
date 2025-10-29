c779bc6af0dbb0bb2bc10d824d7bf9859b205478
G <giovanni@MacBook-Pro-M4.local>
2025-10-29 15:23:26 +0300

    upd

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,19 @@
 <template>
-  <header class="w-full max-w-[430px] mx-auto px-6 pt-6 pb-2 flex items-center justify-between select-none">
+  <header
+    class="w-full max-w-[430px] mx-auto px-6 pt-6 pb-2 flex items-center justify-between select-none z-50 relative"
+  >
     <button class="text-[13px] opacity-75 hover:opacity-100">магазин</button>
-    <h1 class="text-[22px] font-extrabold tracking-tight"><span class="text-pink-400">LOVER</span> CHAINSO</h1>
-    <button class="text-[13px] opacity-75 hover:opacity-100" @click="$emit('open-settings')">
-  настройки
-</button>
+
+    <h1 class="text-[22px] font-extrabold tracking-tight">
+      <span class="text-pink-400">LOVER</span> CHAINSO
+    </h1>
+
+    <!-- 👇 вот эта кнопка -->
+    <button
+      class="text-[13px] opacity-75 hover:opacity-100"
+      @click="$emit('open-settings')"
+    >
+      настройки
+    </button>
   </header>
 </template>
-<script setup></script>
